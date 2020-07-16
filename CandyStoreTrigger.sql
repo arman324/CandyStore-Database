ba5d0c075f27a14e8890003a1452e5db68598961
Arman <riasiarman@yahoo.com>
2020-07-16 16:06:38 +0430

    Creating a new Trigger

--- CandyStoreTrigger.sql
+++ CandyStoreTrigger.sql
@@ -17,3 +17,21 @@ insert into CustomerPhoneNumber values(39,'49-2300000000')
 insert into CustomerPhoneNumber values(39,'230000000000')
 insert into CustomerPhoneNumber values(39,'49-2330')
 select * from CustomerPhoneNumber
+
+
+-- TRIGGER 2
+alter TRIGGER CheckNationalCode
+ on Manager
+ AFTER INSERT
+ AS
+ declare @nationalCode varchar(20);
+ set @nationalCode = (select NationalCode from inserted)
+
+ if (SUBSTRING(@nationalCode,1,1) <> 0)
+    set @nationalCode = CONCAT('0',@nationalCode)
+
+ UPDATE Manager
+ set NationalCode = CONCAT(SUBSTRING(@nationalCode,1,3),CONCAT('-',SUBSTRING(@nationalCode,4,len(@nationalCode))))
+ where ManagerId = (select ManagerId from inserted)
+
+
